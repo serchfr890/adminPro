eaa92acf065f72a07c0bc797c1d43773b7ad140e
Sergio Flores Ramírez <sergio.flores@axity.com>
2021-07-29 01:19:35 -0500

    fin sección 3 - Diseño

---
+++ src/app/pages/no-page-found/no-page-found.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-page-found',
+  templateUrl: './no-page-found.component.html',
+  styleUrls: ['./no-page-found.component.css']
+})
+export class NoPageFoundComponent {
+
+  year = new Date().getFullYear();
+  constructor() { }
+
+}
