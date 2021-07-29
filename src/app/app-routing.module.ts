5383635df45333ebed4a9579a6ebd5f70660f019
Sergio Flores Ramírez <sergio.flores@axity.com>
2021-07-29 02:27:43 -0500

    Se agrega módulo de rutas hijas para Auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: '**', component: NoPageFoundComponent },
 ];
 
@@ -20,6 +17,7 @@ const routes: Routes = [
   exports: [
     RouterModule,
     PagesRoutingModule,
+    AuthRoutingModule,
   ],
 })
 export class AppRoutingModule { }
